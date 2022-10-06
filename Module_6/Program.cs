3071aabb5f5593fa348157bc982b61d72e4e7b54
Nikula Daniil <daniil.nikula@gmail.com>
2022-10-06 18:37:27 +0300

    Добавьте файлы проекта.

---
+++ Module_6/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Module_6
+{
+    class Rectangle
+    {
+        public int a;
+        public int b;
+
+        //Метод
+        public int Square()
+        {
+            return a * b;
+        }
+        //Конструктор 1
+        public Rectangle()
+        {
+            a = 6;
+            b = 4;
+        }
+        //Конструктор 2
+        public Rectangle(int side)
+        {
+            a = side;
+            b = side;
+        }
+        //Конструктор 3
+        public Rectangle(int first, int second)
+        {
+            a = first;
+            b = second;
+        }
+
+        class Pen
+    {
+        public string color;
+        public int cost;
+
+        //Конструктор 1
+        public Pen()
+        {
+            color = "Черный";
+            cost = 100;
+        }
+        //Конструктор 2
+        public Pen(string penColor, int penCost)
+        {
+            color = penColor;
+            cost = penCost;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

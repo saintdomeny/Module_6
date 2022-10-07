f0617d2cb16ed6b57d19dd09fdbc6cbc38f36e0f
Nikula Daniil <daniil.nikula@gmail.com>
2022-10-07 15:29:42 +0300

    Module 6 done

--- Module_6/Program.cs
+++ Module_6/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Module_6
+﻿using System.Drawing;
+
+namespace Module_6
 {
     class Rectangle
     {
@@ -28,8 +30,205 @@
             a = first;
             b = second;
         }
+    }
+    //6.6.1
+    class TrafficLight
+    {
+        private void ChangeColor(string color)
+        {
 
-        class Pen
+        }
+        public string GetColor()
+        {
+            string color = "color";
+            return color;
+        }
+    }
+    //6.6.2
+    class User
+    {
+        private int age;
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                if (value < 18)
+                {
+                    Console.WriteLine("Возраст должен быть не меньше 18");
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+        }
+        private string login;
+        public string Login
+        {
+            get
+            {
+                return login;
+            }
+            set
+            {
+                if(value.Length < 3)
+                {
+                    Console.WriteLine("Логин должен быть не менее 3 символов длиинной");
+                }
+                else
+                {
+                    login = value;
+                }
+            }
+        }
+        private string email;
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                if(!value.Contains('@'))
+                {
+                    Console.WriteLine("Неверный формат адреса электронной почты");
+                }
+                else
+                {
+                    email = value;
+                }
+                    
+            }
+        }
+    }
+    //Example private/public
+    enum TurnDirection
+    {
+        None = 0,
+        Left,
+        Right
+    }
+
+    class Car
+    {
+        private double Fuel;
+
+        private int Mileage;
+
+        private string color;
+
+        private TurnDirection turn;
+
+        public Car()
+        {
+            Fuel = 50;
+            Mileage = 0;
+            color = "White";
+        }
+
+        private void Move()
+        {
+            // Move a kilometer
+            Mileage++;
+            Fuel -= 0.5;
+        }
+
+        private void Turn(TurnDirection direction)
+        {
+            turn = direction;
+        }
+
+        public void FillTheCar()
+        {
+            Fuel = 50;
+        }
+
+        public string GetColor()
+        {
+            return color;
+        }
+
+        public void ChangeColor(string newColor)
+        {
+            if (color != newColor)
+                color = newColor;
+        }
+
+        public bool IsTurningLeft()
+        {
+            return turn == TurnDirection.Left;
+        }
+
+        public bool IsTurningRight()
+        {
+            return turn == TurnDirection.Right;
+        }
+    }
+    //6.5.2
+    /*
+    class Triangle
+    {
+        public int a;
+        public int b;
+        public int c;
+        public int h;
+
+        public double Square()
+        {
+        }
+        public double Perimeter()
+        {
+
+        }
+    }
+    class Circle
+    {
+        public double radius;
+        public double Square()
+        {
+        }
+
+        public double Length()
+        {
+        }
+    }
+    
+    class Square
+    {
+        public int side;
+        public double Square()
+        {
+        }
+        public double Perimeter()
+        {
+
+        }
+    }*/
+    class Bus
+    {
+        //6.3.2
+        public int? Load;
+
+        public void PrintStatus()
+        {
+            if (Load.HasValue)
+            {
+                Console.WriteLine("В авбтобусе {0} пассажиров", Load.Value);
+            }
+            else
+            {
+                Console.WriteLine("Автобус пуст!");
+            }
+        }
+    }
+    class Pen
     {
         public string color;
         public int cost;
@@ -53,5 +252,6 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
     }
 }
